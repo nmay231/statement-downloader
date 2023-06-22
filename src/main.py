b6439827c548dd01ff287a13aa17f422477bb2a9
Noah May <noahmouse2011@gmail.com>
2023-06-22 00:29:10 -0500

    Load a browser with all context remembered

--- src/main.py
+++ src/main.py
@@ -1,2 +1,21 @@
+from pathlib import Path
+
+from playwright.sync_api import sync_playwright
+
+playwright = sync_playwright().start()
+
+
 def main():
-    print("hello world")
+    browser = playwright.chromium.launch(headless=False)
+    auth_context_path = (
+        Path.home() / ".local/share/state_dl/browser_context.json"
+    ).resolve()
+    context = browser.new_context(storage_state=auth_context_path)
+
+    page = context.new_page()
+
+    page.goto("https://github.com/")
+    input()
+    context.storage_state(path=auth_context_path)
+    # page.wait_for_timeout(1000)
+    context.close()
