6e3261fe539b3c1881d6b9d8b61bc57e70a23802
Noah May <noahmouse2011@gmail.com>
2023-06-27 16:43:12 -0500

    Initial snapshot implementation

--- src/main.py
+++ src/main.py
@@ -1,52 +1,124 @@
 import json
 from dataclasses import dataclass
+from datetime import datetime
 from pathlib import Path
 
-from playwright.sync_api import sync_playwright
+from playwright.async_api import async_playwright
 from textual import on
 from textual.app import App, ComposeResult
 from textual.binding import Binding
+from textual.containers import Container, Grid, Horizontal
 from textual.screen import Screen
 from textual.widgets import (
     Button,
+    Checkbox,
+    ContentSwitcher,
     Footer,
+    Input,
+    Label,
+    ListView,
+    OptionList,
     Static,
 )
 
 
+class Browser:
+    # TODO: Context manager? It's easy enough to do, but I don't know if I can
+    #       synchronize a context manager to the lifecycle of a textual component
+    async def start(self, url: str):
+        playwright = await async_playwright().start()
+        browser = await playwright.chromium.launch(headless=False)
+        self._auth_path = (
+            Path.home() / ".local/share/state_dl/browser_context.json"
+        ).resolve()
+        p = self._auth_path
+        self.context = await browser.new_context(storage_state=p if p.exists() else None)
+
+        self.page = await self.context.new_page()
+        await self.page.goto(url)
+
+    async def cleanup(self):
+        self._auth_path.parent.mkdir(parents=True, exist_ok=True)
+        await self.context.storage_state(path=self._auth_path)
+        await self.context.close()
+
+
 @dataclass
-class Procedure:
-    name: str
+class Snapshot:
+    url: str
+    html_content: str
+    time: datetime
 
 
-class SnapshotNewProcedure(Screen):
+class SnapshotNewProcedure(Screen[list[Snapshot]]):
+    snapshots: list[Snapshot]
+
     def compose(self) -> ComposeResult:
-        yield Button("Pop screen", id="pop")
-        yield Button("To new procedure", id="to_new_procedure")
+        self.snapshots = []
+        with ContentSwitcher(initial="start") as switch:
+            self.switch = switch
+            with Container(id="start"):
+                yield Static("Press <Enter> to launch browser")
+                yield Input(placeholder="Initial URL", id="url_input")
+            with Container(id="snapshots"):
+                self.new_snapshot_button = Button("Take Snapshot", id="new_snapshot")
+                yield self.new_snapshot_button
+                self.snapshot_list = Container(id="snapshot_list")
+                yield self.snapshot_list
+                yield Button("Stop", id="stop_snapshot")
 
-    @on(Button.Pressed, "#pop")
-    def pop(self):
+    @on(Button.Pressed, "#new_snapshot")
+    async def new_snapshot(self):
+        snap = Snapshot(
+            self.browser.page.url,
+            await self.browser.page.content(),
+            datetime.now(),
+        )
+        self.snapshots.append(snap)
+        horizontal = Horizontal(Static(snap.url), Static(snap.time.isoformat()))
+        horizontal.styles.height = 1
+        self.snapshot_list.mount(horizontal)
+
+    @on(Button.Pressed, "#stop_snapshot")
+    def stop_snapshot(self):
+        self.dismiss(self.snapshots)
         self.app.pop_screen()
 
-    @on(Button.Pressed, "#to_new_procedure")
-    def to_new_procedure(self):
-        self.app.pop_screen()
-        self.app.push_screen(NewProcedure())
+    @on(Input.Submitted, "#url_input")
+    async def start_snapshots(self):
+        url = self.query_one("#url_input", Input).value
+        self.switch.current = "snapshots"
+        self.new_snapshot_button.focus()
+        self.browser = Browser()
+        await self.browser.start(url)
+
+    async def on_unmount(self):
+        await self.browser.cleanup()
 
 
-class NewProcedure(Screen):
+@dataclass
+class TODOProcedure:
+    ...
+
+
+class NewProcedure(Screen[TODOProcedure]):
+    def __init__(
+        self,
+        name: str | None = None,
+        id: str | None = None,
+        classes: str | None = None,
+        /,
+        snapshot=False,
+    ) -> None:
+        self.snapshot = snapshot
+        super().__init__(name, id, classes)
+
+    def on_mount(self):
+        if self.snapshot:
+            self.app.push_screen(SnapshotNewProcedure())
+
     def compose(self) -> ComposeResult:
-        yield Static("New procedure! (press ctrl+q to quit)")
-
-
-class SubScreen(Screen):
-    def compose(self) -> ComposeResult:
-        yield Static("hello there")
-        yield Button("Back to main menu", id="to_main_menu")
-
-    @on(Button.Pressed, "#to_main_menu")
-    def to_main_menu(self):
-        self.app.pop_screen()
+        yield Static(f"New procedure! {self.snapshot}")
 
 
 class MyApp(App):
@@ -58,40 +130,17 @@ class MyApp(App):
         yield Button("Snapshot Before new procedure", id="to_snapshot_new_procedure")
         yield Footer()
 
-    @on(Button.Pressed, "#to_sub_screen")
-    def put_on_some_sub_screen(self):
-        self.push_screen(SubScreen())
-
     @on(Button.Pressed, "#to_new_procedure")
     def to_new_procedure(self):
-        self.push_screen(NewProcedure())
+        self.push_screen(NewProcedure(snapshot=False))
 
     @on(Button.Pressed, "#to_snapshot_new_procedure")
     def to_snapshot_new_procedure(self):
-        self.push_screen(SnapshotNewProcedure())
+        self.push_screen(NewProcedure(snapshot=True))
 
 
 def main():
     MyApp().run()
-
-
-def main_old():
-    config = json.loads(Path("./tmp_config.json").read_text())["options"][0]
-    print(config)
-    playwright = sync_playwright().start()
-    browser = playwright.chromium.launch(headless=False)
-    auth_path = (Path.home() / ".local/share/state_dl/browser_context.json").resolve()
-    context = browser.new_context(storage_state=auth_path if auth_path.exists() else None)
-
-    page = context.new_page()
-    page.goto(config["website"])
-    table = page.locator("table")
-    print(table)
-
-    auth_path.parent.mkdir(parents=True, exist_ok=True)
-    context.storage_state(path=auth_path)
-    # page.wait_for_timeout(1000)
-    context.close()
 
 
 if __name__ == "__main__":
