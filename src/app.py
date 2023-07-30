bca43f56e37a6d7b1b02d7b5f8173c3bf08936ae
Noah May <noahmouse2011@gmail.com>
2023-07-29 21:31:15 -0500

    Build up to multiple procedures...

--- src/app.py
+++ src/app.py
@@ -31,7 +31,7 @@ from textual.widgets.option_list import Option
 from textual.widgets.selection_list import Selection
 
 from .browser import BrowserWrapper
-from .env import PROCEDURES_DIR
+from .env import PROCEDURES_DIR, ProcedureInfo, config
 
 
 @dataclass
@@ -100,12 +100,8 @@ DEFAULT_PROCEDURE_SNIPPET = (
 ).read_text()
 
 
-@dataclass
-class TODOProcedure:
-    ...
-
-
-class NewProcedure(Screen[TODOProcedure]):
+# TODO: Add a submit button and .dismiss(procedure_info)
+class NewProcedure(Screen[ProcedureInfo]):
     # TODO: How to share browser without stepping on each other's toes?
     _browser: BrowserWrapper | None = None
     module: ModuleType | None = None
@@ -116,6 +112,7 @@ class NewProcedure(Screen[TODOProcedure]):
         id: str | None = None,
         classes: str | None = None,
         /,
+        # procedure_name: str | None = None,
         snapshot=False,
     ) -> None:
         super().__init__(name, id, classes)
@@ -126,6 +123,9 @@ class NewProcedure(Screen[TODOProcedure]):
         # count = len(glob.glob(str(PROCEDURES_DIR)))
         count = 0
         self.procedure_file = PROCEDURES_DIR / f"m{count}.py"
+        self.procedure_file = PROCEDURES_DIR / "0test with spaces.py"
+        if not self.procedure_file.exists():
+            self.procedure_file.write_text(DEFAULT_PROCEDURE_SNIPPET)
 
     def on_mount(self):
         if self.to_snapshot:
@@ -192,6 +192,8 @@ class NewProcedure(Screen[TODOProcedure]):
         self.procedure_file.parent.mkdir(parents=True, exist_ok=True)
         self.procedure_file.write_text(self.editor.text)
 
+        # TODO: Since an imported module technically is cached, and we can import more than one module, I might need to keep track of all imported modules, not just the current one, so I don't get any out of date cache hits
+        # I can check if the module name is in sys.modules and if it is then reload it
         if self.module:
             importlib.reload(self.module)
         else:
@@ -199,7 +201,7 @@ class NewProcedure(Screen[TODOProcedure]):
                 name=self.procedure_file.stem,
                 package=PROCEDURES_DIR.stem,
             )
-        # TODO: Figure out how to flush updates so this actually works
+        # TODO: Figure out how to flush updates so this actually works halfway through this function
         self.name_label.update(f"{self.module.PROCEDURE_NAME} <PENDING>")
         self.name_label.refresh()
 
@@ -291,10 +293,42 @@ class MyApp(App):
     ]
     CSS_PATH = ["app.tcss"]
 
+    procedures = dict[str, ProcedureInfo | None]()
+
     def compose(self) -> ComposeResult:
-        yield Button("New procedure", id="to_new_procedure")
-        yield Button("Snapshot Before new procedure", id="to_snapshot_new_procedure")
-        yield Footer()
+        with Horizontal():
+            yield Button("New procedure", id="to_new_procedure")
+            yield Button("Snapshot before new procedure", id="to_snapshot_new_procedure")
+            self.edit_procedure_button = Button(
+                "Edit procedure", id="edit_procedure", disabled=True
+            )
+            yield self.edit_procedure_button
+        self.procedure_list = OptionList(id="procedure_list")
+        yield self.procedure_list
+        yield Footer()  # TODO: Footer needs to be on all relevant screens
+
+    def on_mount(self):
+        self._load_procedures()
+
+    def _load_procedures(self):
+        self.procedure_list.clear_options()
+        all_procedures = {**config.procedures}
+
+        for procedure in PROCEDURES_DIR.glob("*.py"):
+            name = procedure.stem
+            proc = all_procedures.pop(name, None)
+            self.procedures[name] = proc
+
+            if proc is None:
+                self.procedure_list.add_option(
+                    Option(f"{name}.py <Untracked Procedure!>", id=name)
+                )
+            else:
+                self.procedure_list.add_option(Option(proc.display_name, id=name))
+
+        for name, procedure in all_procedures.items():
+            self.procedures[name] = procedure
+            self.procedure_list.add_option(Option(f"{name} <File missing!>", id=None))
 
     @on(Button.Pressed, "#to_new_procedure")
     def to_new_procedure(self):
@@ -303,3 +337,21 @@ class MyApp(App):
     @on(Button.Pressed, "#to_snapshot_new_procedure")
     def to_snapshot_new_procedure(self):
         self.push_screen(NewProcedure(snapshot=True))
+
+    @on(OptionList.OptionSelected, "#procedure_list")
+    async def snapshot_list_selected(self, selected: OptionList.OptionSelected) -> None:
+        self.current_proc_name = selected.option_id  # I hate this...
+        print("OPTION", selected.option_id)
+        if selected.option_id is None:
+            self.edit_procedure_button.disabled = True
+            return
+        assert selected.option_id in self.procedures, "Forgot to update self.procedures"
+        self.edit_procedure_button.disabled = not (self.procedures.get(selected.option_id))
+
+    @on(Button.Pressed, "#edit_procedure")
+    async def edit_procedure(self) -> None:
+        name = self.current_proc_name
+        assert name and name in self.procedures, "Forgot to update self.procedures or procname"
+
+        # TODO: Make NewProcedure => EditProcedure
+        # self.push_screen(NewProcedure(procedure_name=name))
--- src/env.py
+++ src/env.py
@@ -1,6 +1,34 @@
 import sys
+from enum import Enum
 from pathlib import Path
+
+from pydantic import BaseModel
 
 CONFIG_PATH = (Path.home() / ".local/share/state_dl").resolve()
 PROCEDURES_DIR = CONFIG_PATH / "procedure_scripts"
+# TODO: Is this even needed?
 sys.path.append(str(PROCEDURES_DIR))  # TODO: Better place to do this
+
+
+class Config(BaseModel):
+    procedures: dict[str, "ProcedureInfo"]
+    contexts: dict[str, "ContextInfo"]
+
+
+class ProcedureInfo(BaseModel):
+    display_name: str
+
+
+class BrowserEnum(str, Enum):
+    chromium = "chromium"
+    firefox = "firefox"
+    webkit = "webkit"
+
+
+class ContextInfo(BaseModel):
+    display_name: str
+    browser: BrowserEnum
+
+
+# TODO: Load elsewhere
+config = Config.model_validate_json((CONFIG_PATH / "data.json").read_text())
